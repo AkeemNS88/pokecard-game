17dda8ebb44b1327cf1d829f25dfc4e30aa89bc7
Akeem <AkeemS11788@gmail.com>
2022-07-13 17:27:20 -0400

    final commit styling finished

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,11 +4,16 @@ import './Pokedex.css';
 
 
 function Pokedex({pokemon, exp, isWinner}) {
+    let title;
+    if(isWinner){
+        title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+    } else {
+        title = <h1 className="Pokedex-loser">Losing Hand</h1>
+    }
     return (
         <div className="Pokedex">
-            <h1>Pokedex!</h1>
+            {title}
             <p>Total Experience: {exp}</p>
-            <p>{isWinner ? 'WINNER!' : 'LOSER!'}</p>
             <div className="Pokedex-cards">
             {pokemon.map((p) => (
                 <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
